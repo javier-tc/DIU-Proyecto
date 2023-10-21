99a752a9ee1f71fac2f2ef9e74759ec002796e78
javier-tp <javier.tapia.cvl@gmail.com>
2023-10-21 16:34:01 -0300

    ajuste navbar

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -3,9 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-danger">
-      <NavLink className="navbar-brand text-white" to="/">Meal Planner</NavLink>
-    </nav>
+    <div className='navbar'>
+      <nav className="navbar fixed-top bg-danger">
+        <NavLink className="navbar-brand text-white" to="/">Meal Planner</NavLink>
+      </nav>
+    </div>
   );
 };
 
