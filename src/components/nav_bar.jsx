5993891625919ff1b5ba6038280eed48ef4932b6
javier-tp <javier.tapia.cvl@gmail.com>
2023-11-15 09:21:52 -0300

    update icono

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import mealPlannerLogo from '../../public/images/icono_proyecto_diu_3.png';
 
 const NavBar = () => {
 	return (
 		<div className='navbar-container'>
 			<nav className='navbar'>
-				<NavLink className='navbar-link' to="/" exact>Meal Planner</NavLink>
+				<NavLink to="/" exact>
+					<img src={mealPlannerLogo} alt="Meal Planner" className='navbar-logo' />
+				</NavLink>
 				<NavLink className='navbar-link' to="/calendar">Calendario</NavLink>
 			</nav>
 		</div>
